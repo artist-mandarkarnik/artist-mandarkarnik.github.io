dd584d05d99152226541b476d48c2bd7f9e62003
Mandar Karnik <mandarkarnik@ymail.com>
2025-12-26 03:00:46 -0500

    script.js
    add lightbox functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll(".grid img");
+const lightbox = document.getElementById("lightbox");
+const lbImg = document.getElementById("lightbox-img");
+const lbTitle = document.getElementById("lightbox-title");
+const lbSize = document.getElementById("lightbox-size");
+const lbDesc = document.getElementById("lightbox-desc");
+const closeBtn = document.getElementById("close");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lbImg.src = img.src;
+    lbTitle.textContent = img.dataset.title;
+    lbSize.textContent = img.dataset.size;
+    lbDesc.textContent = img.dataset.desc;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
